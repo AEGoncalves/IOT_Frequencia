76b2a862bfb09ea22925802978672103be733353
André Gonçalves <150437231+AEGoncalves@users.noreply.github.com>
2026-01-15 16:11:38 +0530

    frequencia2025

---
+++ gps2Frequencia2025/gps-sensor-trigger/iot-hub-trigger/__init__.py
@@ -0,0 +1,31 @@
+import logging
+
+import azure.functions as func
+
+import json
+import os
+from azure.iot.hub import IoTHubRegistryManager
+from azure.iot.hub.models import CloudToDeviceMethod
+
+def main(event: func.EventHubEvent):    
+    body = json.loads(json.loads(event.get_body().decode('utf-8')))
+    device_id = event.iothub_metadata['connection-device-id']
+
+    logging.info(f'Received message: {body} from {device_id}')
+
+    longitude = body['long']
+
+    if longitude > 0:
+        direct_method = CloudToDeviceMethod(method_name='led_vermelho_on', payload='{}')
+    else:
+        direct_method = CloudToDeviceMethod(method_name='led_verde_on', payload='{}')
+
+    logging.info(f'Sending direct method request for {direct_method.method_name} for device {device_id}')
+    
+    registry_manager_connection_string = os.environ['REGISTRY_MANAGER_CONNECTION_STRING']
+    registry_manager = IoTHubRegistryManager(registry_manager_connection_string)
+
+    registry_manager.invoke_device_method(device_id, direct_method)
+
+    logging.info('Direct method request sent!')
+
